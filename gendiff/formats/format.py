ff133cc53f4acbd162157022703bfdc9b128fe70
trun <e.s.trunov@gmail.com>
2025-01-02 18:18:26 +0700

    add gendiff

--- gendiff/formats/format.py
+++ gendiff/formats/format.py
@@ -5,9 +5,9 @@ from gendiff.formats import create_json
 
 def format_diff(diff, format_name):
     if format_name == 'stylish':
-        return stylish.stylish(diff)
+        return stylish.create_stylish(diff)
     elif format_name == 'plain':
-        return plain.plain(diff)
+        return plain.create_plain(diff)
     elif format_name == 'json':
         return create_json.create_json(diff)
     else:
