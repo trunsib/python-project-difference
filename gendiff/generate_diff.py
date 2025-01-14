c640f55e10f07dff64a7b7540a61733599ccff32
trun <e.s.trunov@gmail.com>
2025-01-14 15:19:08 +0700

    add gendiff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,6 +1,6 @@
 from gendiff.diff import get_diff
 from gendiff.data import get_data
-from gendiff.formats import format
+from gendiff.formats import formatter
 
 
 def generate_diff(first_file_path, second_file_path, format_name='stylish'):
@@ -8,4 +8,4 @@ def generate_diff(first_file_path, second_file_path, format_name='stylish'):
     data2 = get_data(second_file_path)
     diff = get_diff(data1, data2)
 
-    return format(diff, format_name)
+    return formatter(diff, format_name)
