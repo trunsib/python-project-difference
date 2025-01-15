1b1c8727f2ec66c02db33a02f10ada564f5018f4
trun <e.s.trunov@gmail.com>
2025-01-15 15:29:44 +0700

    add gendiff

--- gendiff/diff.py
+++ gendiff/diff.py
@@ -1,23 +1,39 @@
 def get_diff(data1, data2):
-    res = []
-    keys = sorted(data1.keys() | data2.keys())
+
+    diff = []
+    keys = sorted(set(data1.keys()) | set(data2.keys()))
+
     for key in keys:
-        node = {'name': key}
-        if key not in data1:
-            node['status'] = 'added'
-            node['data'] = data2[key]
-        elif key not in data2:
-            node['status'] = 'deleted'
-            node['data'] = data1[key]
-        elif type(data1[key]) is dict and type(data2[key]) is dict:
-            node['status'] = 'nested'
-            node['children'] = get_diff(data1[key], data2[key])
+        if key not in data1.keys():
+            diff.append({
+                'key': key,
+                'type': 'add',
+                'value': data2[key]
+            })
+        elif key not in data2.keys():
+            diff.append({
+                'key': key,
+                'type': 'remove',
+                'value': data1[key]
+            })
+        elif isinstance(data1.get(key), dict) and \
+                isinstance(data2.get(key), dict):
+            diff.append({
+                'key': key,
+                'type': 'children',
+                'value': get_diff(data1[key], data2[key])
+            })
         elif data1[key] == data2[key]:
-            node['status'] = 'not changed'
-            node['data'] = data1[key]
+            diff.append({
+                'key': key,
+                'type': 'same',
+                'value': data1[key]
+            })
         else:
-            node['status'] = 'changed'
-            node['data before'] = data1[key]
-            node['data after'] = data2[key]
-        res.append(node)
-    return res
+            diff.append({
+                'key': key,
+                'type': 'change',
+                'value': data1[key],
+                'new_value': data2[key]
+            })
+    return diff
